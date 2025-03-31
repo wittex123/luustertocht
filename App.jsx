030d7d9046d0e01585ef8cbe373415969d2d961b
wittex123 <job@wezenspyk.nl>
2025-03-31 22:28:19 +0200

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
+      <h1>Luustertocht</h1>
+      <p>Welkom bij de audiotour!</p>
+    </div>
+  );
+}
